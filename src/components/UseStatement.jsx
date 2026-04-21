3327a2e34a9820692d5ea44e68f5532f8f74d452
Ragul priyan <ragulpriyan2731@gmail.com>
2026-04-21 10:37:20 +0530

    usestate_array_objects

--- src/components/UseStatement.jsx
+++ src/components/UseStatement.jsx
@@ -1,13 +1,36 @@
 import { useState } from "react"
 
 const UseStatement=() =>{
-    const [dosti, setdosti] = useState(["ragul","sha","narayan","krishna"])
+    // const [dosti, setdosti] = useState(["ragul","sha","narayan","krishna"])
+    // // add,update,remove from an array
+    // const Adddostiname = ()=> setdosti([...dosti,"ragul priyan","sha"])
+    // const Removedostiname = ()=> setdosti((dosti.filter((test) => test !== "sha")));
+    // const Updatedostiname = ()=> {setdosti(dosti.map(f=>f === "sha" ? "shainthuja": f))}
+    const [film, setfilm] = useState({
+        name: "fince",
+        ratings: 7
+      })
+      const handlechange = ()=>setfilm({...film, ratings:9})
+      
 
   return (
-    <div>
+      <div>
+        <h1>{film.name}</h1>
+        <p>{film.ratings}</p>
+        <button onClick={handlechange}>changeratings</button>
 
-    <button ></button>
-    </div>
+
+
+      </div>
+
+    // <div>
+    //   {dosti.map((f, index)=>(
+    //   <li key ={index}>{f}</li>
+    //   ))}
+    // <button onClick={Adddostiname}>Adddosti</button>
+    // <button onClick={Removedostiname}>Removedostiname</button>
+    // <button onClick={Updatedostiname}>Updatename</button>
+    // </div>
   )
 }
 
