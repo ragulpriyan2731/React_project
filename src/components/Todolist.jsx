cb7febd46337d119f16f82ad497b25baacd9182f
Ragul priyan <ragulpriyan2731@gmail.com>
2026-04-23 19:01:32 +0530

    Todolist_project_tailwindcss_styles

--- src/components/Todolist.jsx
+++ src/components/Todolist.jsx
@@ -18,21 +18,72 @@ const Todolist =()=>{
         // setTodos([]) all Empty
         setTodos(Todos.slice(0,-1)); 
     }
+    const deleteTodo = (indexToDelete) => {
+    const updatedTodos = Todos.filter((_, index) => index !== indexToDelete);
+    setTodos(updatedTodos);
+};
 
  return(
-        <>
-        <h1>Todolist</h1>
-        <form onSubmit={formsubmit}>
-        <input type="text" value={Input} onChange={inputchange} placeholder="Add a todolist" />
-        <button type="submit">Submit</button>
-        <button onClick={cleartodolist} disabled={Todos.length === 0}>Clear</button>
-        </form>
-        <ul>
-            {Todos.map((todo, index)=>(
-                <p key={index}>{todo}</p>
+    <>
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <div className="bg-white shadow-lg rounded-2xl p-6 w-full max-w-md">
+    
+        <h1 className="text-2xl font-bold text-center text-gray-800 mb-6">
+         Todo List
+         </h1>
+
+        <form onSubmit={formsubmit} className="space-y-4">
+      
+         <input
+        type="text"
+        value={Input}
+        onChange={inputchange}
+        placeholder="Add a todo"
+        className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+
+    <div className="flex gap-3">
+        <button
+          type="submit"
+          className="flex-1 bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition duration-200"
+        >
+          Submit
+        </button>
+
+        <button
+          onClick={cleartodolist}
+          disabled={Todos.length === 0}
+          className={`flex-1 py-2 rounded-lg text-white transition duration-200 ${
+            Todos.length === 0
+              ? "bg-gray-400 cursor-not-allowed"
+              : "bg-red-500 hover:bg-red-600"
+          }`}
+        >
+          Clear
+        </button>
+    </div>
+        
+            <ul className="mt-6 space-y-3">
+                {Todos.map((todo, index) => (
+             <li
+            key={index}
+            className="flex items-center justify-between bg-white px-4 py-2 rounded-lg shadow-sm border border-gray-200 hover:shadow-md transition duration-200"
+            >
+            <span className="text-gray-700">{todo}</span>
+            <button
+            onClick={() => deleteTodo(index)}
+            className="bg-red-500 text-white px-3 py-1 rounded-md hover:bg-red-600 transition duration-200"
+            >Delete</button>
+            </li>
             ))}
-        </ul>
-         </>
-    )
+                </ul>
+        
+
+        </form>
+        
+    </div>
+    </div>
+</>
+) 
 }
 export default Todolist
